2afd6c020c3186fc6534cbf28ebaebcc2c8bf5f6
Daniel Kimball <danielyoungkimball@outlook.com>
2025-06-23 13:02:40 -0700

    Add pages

--- src/app/data-deletion/page.tsx
+++ src/app/data-deletion/page.tsx
@@ -3,7 +3,7 @@ export default function DataDeletion() {
     <div className="bg-parchment min-h-screen flex flex-col items-center justify-center text-center p-4">
       <h1 className="text-3xl font-bold mb-4 text-espresso">Data Deletion Instructions</h1>
       <p className="max-w-2xl text-lg mb-6 text-richBlack">
-        If you would like to request the deletion of your personal data from Alo! Coffee & Bakery, please email us at <a href="mailto:alos.coffee@outlook.com" className="underline">alos.coffee@outlook.com</a> with the subject line "Data Deletion Request". We will process your request promptly and confirm once your data has been deleted from our systems.
+        If you would like to request the deletion of your personal data from Alo! Coffee & Bakery, please email us at <a href="mailto:alos.coffee@outlook.com" className="underline">alos.coffee@outlook.com</a> with the subject line &quot;Data Deletion Request&quot;. We will process your request promptly and confirm once your data has been deleted from our systems.
       </p>
     </div>
   );
